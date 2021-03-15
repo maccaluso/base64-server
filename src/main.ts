b8216ea04ca76e3e160a569cb16766e76f1d74b8
Gianluca Macaluso <maccaluso@gmail.com>
2021-03-15 16:06:01 +0100

    set base nest url global prefix

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,7 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.setGlobalPrefix('api/base64-server');
   await app.listen(3101);
 }
 bootstrap();

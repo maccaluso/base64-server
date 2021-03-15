7f50e3292882b31918124870ae25f94c79fb8bd6
Gianluca Macaluso <maccaluso@gmail.com>
2021-03-15 17:32:18 +0100

    decoder endpoint

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,12 +1,30 @@
-import { Controller, Get } from '@nestjs/common';
+import { Body, Controller, Get, Post } from '@nestjs/common';
 import { AppService } from './app.service';
+
+import { encode } from 'node-base64-image';
 
 @Controller()
 export class AppController {
+  private options = {
+    string: true,
+    headers: {
+      'User-Agent': 'base64-server',
+    },
+  };
+
   constructor(private readonly appService: AppService) {}
 
   @Get()
-  getHello(): string {
+  async getHello(): Promise<any> {
     return this.appService.getHello();
+  }
+
+  @Post()
+  async returnBase64FromUrl(
+    @Body() body: { url: string; imageType: string },
+  ): Promise<{ base64Image: string }> {
+    const image = await encode(body.url, this.options);
+    return { base64Image: `data:image/${body.imageType};base64,` + image };
+    // return { base64Image: image as string };
   }
 }

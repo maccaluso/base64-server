66ad2ed6b86993665279590075c0182f2b47a49e
Gianluca Macaluso <maccaluso@gmail.com>
2021-03-16 10:10:41 +0100

    cors - localhost only

--- src/main.ts
+++ src/main.ts
@@ -4,7 +4,7 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.setGlobalPrefix('api/base64-server');
-  app.enableCors({ origin: false });
+  app.enableCors({ origin: 'http://localhost:3000' });
   await app.listen(3101);
 }
 bootstrap();

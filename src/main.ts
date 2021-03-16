6397e90714f849f2c2fe5ef3581c0f124d717437
Gianluca Macaluso <maccaluso@gmail.com>
2021-03-16 10:08:00 +0100

    apertura cors

--- src/main.ts
+++ src/main.ts
@@ -4,6 +4,7 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.setGlobalPrefix('api/base64-server');
+  app.enableCors({ origin: false });
   await app.listen(3101);
 }
 bootstrap();

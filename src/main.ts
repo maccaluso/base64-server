b40d4b7f7d91ba13d3dce6f9dc8fa12c3e6d64e7
Gianluca Macaluso <maccaluso@gmail.com>
2021-03-19 20:17:02 +0100

    CORS enabled for cooperative-press.eu

--- src/main.ts
+++ src/main.ts
@@ -4,7 +4,9 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.setGlobalPrefix('api/base64-server');
-  app.enableCors({ origin: 'http://localhost:3000' });
+  app.enableCors({
+    origin: ['http://localhost:3000', 'https://cooperative-press.eu/'],
+  });
   await app.listen(3101);
 }
 bootstrap();

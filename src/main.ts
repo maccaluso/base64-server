1e475c977afa545632264d78aaf66992c5e04324
Gianluca Macaluso <maccaluso@gmail.com>
2021-03-19 20:26:52 +0100

    fix whitelist domains

--- src/main.ts
+++ src/main.ts
@@ -5,7 +5,7 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.setGlobalPrefix('api/base64-server');
   app.enableCors({
-    origin: ['http://localhost:3000', 'https://cooperative-press.eu/'],
+    origin: ['http://localhost:3000', 'https://cooperative-press.eu'],
   });
   await app.listen(3101);
 }
